9e833c473e4d2525c8ca8169df74901987f00e10
noedelorme <lenoedelorme@gmail.com>
2022-03-07 20:03:00 +0100

    the great beginning

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+/**************************************
+  MODULE IMPORT
+**************************************/
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const config = require('./config.json');
+
+
+/**************************************
+  INITIALISATION
+**************************************/
+const app = express();
+app.use(express.static(path.join(__dirname, '../frontend/build/')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+/**************************************
+  ROOTER SYSTEM
+**************************************/
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../frontend/build/', 'index.html'));
+});
+
+app.get('*', function(req, res){
+  res.send('404');
+});
+
+app.listen(config.port, config.adress);
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="hello">Hello world!</div>
+  );
+}
+
+export default App;
