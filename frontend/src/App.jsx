5b4c04feda0bebc878fae389628c35a8675f14cb
noedelorme <lenoedelorme@gmail.com>
2022-03-12 00:05:35 +0100

    the great and productive night

---
+++ frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import './css/App.scss';
+
+const world = require("./world.json");
+
+function Country(props){
+  return (
+    <div className="country">
+      <div className="country-content clearfix">
+        <div className="flag">
+          <img src={props.country.flag} />
+        </div>
+        <div className="country-info">
+          <span className="country-name">{props.country.name} <span className="population">({props.country.population})</span></span>
+          <span className="capital">{props.country.capital}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Continent(props){
+  return (
+    <div className="continent">
+      <div className="continent-name">{props.continent.name} <span className="population">({props.continent.population})</span></div>
+      {props.continent.countries.map((item, index) => {
+        return <Country key={index} country={item} />;
+      })}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="list">
+      {world.continents.map((item, index) => {
+        return <Continent key={index} continent={item} />;
+      })}
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.scss';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
